9c44e564eb6bcf746cd9bceb57dc195759e1dabe
Rebecca Cole <coco_1981@hotmail.com>
2026-03-21 11:45:32 +1000

    fix: remove import

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 import getCurrentUser from "../../api/getCurrentUser";
 import { fetchNotificationSummary } from "../../api/notifications";
 import { useAuthStatus } from "../../hooks/useAuthStatus";
-import "./NavBar.css"; 
 
 import bfLogo from "../../assets/PodFlow.png";
 
